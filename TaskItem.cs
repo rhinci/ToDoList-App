39f0735e2025dae6b344625426558cf9b79ce924
rhinci <dashalupanova@gmail.com>
2025-09-26 09:30:22 +1000

    TaskItem class

---
+++ TaskItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class TaskItem
+    {
+        public bool IsCompleted { get; set; } 
+        public string Title { get; set; }
+        public string Category { get; set; } 
+        public DateTime DueDate { get; set; }
+        public string Description { get; set; } 
+        public int Priority { get; set; }
+    }
+}
