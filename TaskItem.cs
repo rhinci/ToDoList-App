5f529d077dc71fb333336c4f24a7cfc2474abbbc
rhinci <dashalupanova@gmail.com>
2025-09-26 12:29:21 +1000

    date picker

--- TaskItem.cs
+++ TaskItem.cs
@@ -14,5 +14,10 @@ namespace ToDoList
         public DateTime DueDate { get; set; }
         public string Description { get; set; } 
         public int Priority { get; set; }
+
+        public TaskItem() 
+        {
+            DueDate = DateTime.Now;
+        }
     }
 }
